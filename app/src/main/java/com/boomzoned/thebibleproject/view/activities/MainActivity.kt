5cb8b3ff247a41046ecee6e118666b4584e8f122
Jeevan Philip <jgphilip@gmail.com>
2020-04-16 16:52:49 -0400

    Added folder structure for views,viewmodels and di

--- app/src/main/java/com/boomzoned/thebibleproject/view/activities/MainActivity.kt
+++ app/src/main/java/com/boomzoned/thebibleproject/view/activities/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.boomzoned.thebibleproject
+package com.boomzoned.thebibleproject.view.activities
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.boomzoned.thebibleproject.R
 
 import kotlinx.android.synthetic.main.activity_main.*
 
