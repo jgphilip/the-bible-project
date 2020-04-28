bef9e591c6d943f212e38b6d9713ef7ef7fc34d7
Jeevan Philip <jgphilip@gmail.com>
2020-04-27 22:58:17 -0400

    Updated Extensions.kt with inTransaction and openActivity

--- app/src/main/java/com/boomzoned/thebibleproject/utils/Extensions.kt
+++ app/src/main/java/com/boomzoned/thebibleproject/utils/Extensions.kt
@@ -1,10 +1,27 @@
 package com.boomzoned.thebibleproject.utils
 
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 
 fun ViewGroup.inflate(@LayoutRes layoutRes: Int, attachToRoot: Boolean = false): View {
     return LayoutInflater.from(context).inflate(layoutRes, this, attachToRoot)
+}
+
+fun FragmentManager.inTransaction(func: FragmentTransaction.() -> Unit) {
+    val fragmentTransaction = beginTransaction()
+    fragmentTransaction.func()
+    fragmentTransaction.commit()
+}
+
+fun <T> Context.openActivity(it: Class<T>, extras: Bundle.() -> Unit = {}) {
+    val intent = Intent(this, it)
+    intent.putExtras(Bundle().apply(extras))
+    startActivity(intent)
 }
