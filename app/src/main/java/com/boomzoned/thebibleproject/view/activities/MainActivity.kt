328d2a767d5f7a375a36bb6efe2993de8b19be78
Jeevan Philip <jgphilip@gmail.com>
2020-04-30 23:17:28 -0400

    Cleanup code

--- app/src/main/java/com/boomzoned/thebibleproject/view/activities/MainActivity.kt
+++ app/src/main/java/com/boomzoned/thebibleproject/view/activities/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+//        setSupportActionBar(toolbar)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
--- app/src/main/java/com/boomzoned/thebibleproject/view/fragments/SplashScreenFragment.kt
+++ app/src/main/java/com/boomzoned/thebibleproject/view/fragments/SplashScreenFragment.kt
@@ -7,10 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.boomzoned.thebibleproject.R
+
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+
 /**
  * A simple [Fragment] subclass.
  * Use the [SplashScreenFragment.newInstance] factory method to
@@ -29,8 +31,10 @@ class SplashScreenFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_splash_screen, container, false)
     }
@@ -45,12 +49,13 @@ class SplashScreenFragment : Fragment() {
          * @return A new instance of fragment SplashScreenFragment.
          */
         // TODO: Rename and change types and number of parameters
-        @JvmStatic fun newInstance(param1: String, param2: String) =
-                SplashScreenFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            SplashScreenFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
                 }
+            }
     }
 }
