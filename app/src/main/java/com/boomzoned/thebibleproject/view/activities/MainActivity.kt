e4be1df4c81db73407d826ede05d531bd437276c
Jeevan Philip <jgphilip@gmail.com>
2020-05-01 23:23:34 -0400

    Splash screen related changes

--- app/src/main/java/com/boomzoned/thebibleproject/view/activities/MainActivity.kt
+++ app/src/main/java/com/boomzoned/thebibleproject/view/activities/MainActivity.kt
@@ -13,9 +13,9 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        setSupportActionBar(toolbar)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
---
+++ app/src/main/java/com/boomzoned/thebibleproject/view/activities/SplashActivity.kt
@@ -0,0 +1,31 @@
+package com.boomzoned.thebibleproject.view.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.boomzoned.thebibleproject.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Make sure this is before calling super.onCreate
+        setTheme(R.style.AppTheme)
+        super.onCreate(savedInstanceState)
+        scheduleSplashScreen()
+    }
+
+    private fun scheduleSplashScreen() {
+        val splashScreenDuration = getSplashScreenDuration()
+        Handler().postDelayed(
+            {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            },
+            splashScreenDuration
+        )
+    }
+
+    private fun getSplashScreenDuration() = 2000L
+}
